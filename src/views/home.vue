c70a8d0762d9bfb143e5428d658f5e43426aa958
1220x <1960708821@qq.com>
2020-12-24 14:26:24 +0800

    新建工程

---
+++ src/views/home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        {{ test }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home',
+    data() {
+        return {
+            test: '主页----测试'
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
